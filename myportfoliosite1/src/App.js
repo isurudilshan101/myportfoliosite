4f349dc31d767396525d97a0ab7be4129edd8aff
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-12 12:53:00 +0530

    start

---
+++ myportfoliosite1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
