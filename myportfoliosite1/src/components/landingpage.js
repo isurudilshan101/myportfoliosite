48d927f3445fcb5e6c181298f85016498c73c3ad
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-12 18:15:22 +0530

    route

--- myportfoliosite1/src/components/landingpage.js
+++ myportfoliosite1/src/components/landingpage.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
   render() {
     return (
       <div>
-         <h1>Landingpage Page</h1>
+         <h1>Landing Page</h1>
       </div>
     );
   }
---
+++ myportfoliosite1/src/components/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+import Landingpage from './landingpage';
+import AbouteMe from './aboutme';
+import Contact from './contact';
+import Projects from './projects';
+import Resume from './resume';
+
+ const Main=()=>(
+    <Switch>
+        <Route exact path="/" component={Landingpage}/>
+        <Route path="/aboutme" component={AbouteMe}/>
+        <Route path="/projects" component={Projects}/>
+        <Route path="/resume" component={Resume}/>
+        <Route path="/contact" component={Contact}/>
+
+
+    </Switch>
+ )
+
+ export default Main;
