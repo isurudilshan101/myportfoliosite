06641b66e95ff0015015c043df6004034f6d2f64
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-12 19:25:15 +0530

    app.css

--- myportfoliosite1/src/App.js
+++ myportfoliosite1/src/App.js
@@ -10,7 +10,7 @@ class App extends Component{
     return(
       <div className="demo-big-content">
       <Layout>
-          <Header title="Title" scroll>
+          <Header className="header-color" title="Title" scroll>
               <Navigation>
                   <Link to="/resume">Resume</Link>
                   <Link to="/aboutme">About Me</Link>
@@ -21,10 +21,10 @@ class App extends Component{
           </Header>
           <Drawer title="Title">
               <Navigation>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
+              <Link to="/resume">Resume</Link>
+                  <Link to="/aboutme">About Me</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/contact">Contact</Link>
                 
               </Navigation>
           </Drawer>
