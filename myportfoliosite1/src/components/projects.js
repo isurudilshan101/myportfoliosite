fbf2882995876a754f37094878692698e3a3ccc0
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-13 21:39:03 +0530

    card

--- myportfoliosite1/src/components/projects.js
+++ myportfoliosite1/src/components/projects.js
@@ -11,23 +11,62 @@
         toggleCategories(){
             if(this.state.activeTab===0){
                 return(
+                <div>
+                    {/* project 1 */}
                     <Card shadow={5} style={{minwidth:'450',margin:'auto'}}>
-                        <CardTitle style={{color:'#fff',height:'176px',background: 'url(https://www.themightycribb.com/wp-content/uploads/2020/07/react-logo.png) center/cover'}}>React Project</CardTitle>
+                        <CardTitle style={{color:'#fff',height:'176px',background: 'url(https://www.themightycribb.com/wp-content/uploads/2020/07/react-logo.png) center/cover'}}>React Project #1</CardTitle>
                         <CardText>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                         </CardText>
                         <CardActions border>
                     <Button colored>Github</Button>
                     <Button colored>CodePen</Button>
-                    <Button colored>LiveDemo</Button>
+                    <Button colored>Live Demo</Button>
 
                         </CardActions>
                     <CardMenu style={{color:'#fff'}}>
                     <iconButton name="share"/>
                     </CardMenu>
                     </Card>
+
+                      {/* project 2 */}
+                    <Card shadow={5} style={{minwidth:'450',margin:'auto'}}>
+                    <CardTitle style={{color:'#fff',height:'176px',background: 'url(https://www.themightycribb.com/wp-content/uploads/2020/07/react-logo.png) center/cover'}}>React Project #2</CardTitle>
+                    <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    </CardText>
+                    <CardActions border>
+                    <Button colored>Github</Button>
+                    <Button colored>CodePen</Button>
+                    <Button colored>Live Demo</Button>
+
+                    </CardActions>
+                    <CardMenu style={{color:'#fff'}}>
+                    <iconButton name="share"/>
+                    </CardMenu>
+                    </Card>
+
+                    {/* project 3 */}
+                    <Card shadow={5} style={{minwidth:'450',margin:'auto'}}>
+                        <CardTitle style={{color:'#fff',height:'176px',background: 'url(https://www.themightycribb.com/wp-content/uploads/2020/07/react-logo.png) center/cover'}}>React Project #3</CardTitle>
+                        <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        </CardText>
+                        <CardActions border>
+                    <Button colored>Github</Button>
+                    <Button colored>CodePen</Button>
+                    <Button colored>Live Demo</Button>
+
+                        </CardActions>
+                    <CardMenu style={{color:'#fff'}}>
+                    <iconButton name="share"/>
+                    </CardMenu>
+                    </Card>
+                    </div>
                 )
-            }else if(this.state.activeTab===1){
+            }
+            
+            else if(this.state.activeTab===1){
                 return(
                     <div><h1>This is Angular</h1></div>
                 )
