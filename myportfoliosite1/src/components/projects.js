3afc4199f0ca33e96325685e8cc72686e31f318e
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-13 11:29:01 +0530

    tabs

--- myportfoliosite1/src/components/projects.js
+++ myportfoliosite1/src/components/projects.js
@@ -7,6 +7,15 @@
             super(props);
             this.state={activeTab:0};
         }
+
+        toggleCategories(){
+            if(this.state.activeTab===0){
+                return(
+                    <div><h1>This is React</h1></div>
+                )
+            }
+        }
+
    render() {
      return (
        <div className="category-tabs">
