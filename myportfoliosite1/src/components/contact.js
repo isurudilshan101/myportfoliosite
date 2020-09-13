5ccdd51227ae8c4762450aaad8b11c3af377af55
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-13 22:20:32 +0530

    card

--- myportfoliosite1/src/components/contact.js
+++ myportfoliosite1/src/components/contact.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import {Grid,Cell} from 'react-mdl';
+
  class Contact extends Component {
   render() {
     return (
       <div>
-        <h1>Contact page</h1>
+       <Grid className="demo-grid-1">
+        <Cell col={6}>half 1</Cell>
+        <Cell col={6}>half 2</Cell>
+        
+    </Grid>
       </div>
     );
   }
