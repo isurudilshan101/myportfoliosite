2c14d1c493187693eef808e8cb5c16483ff87d54
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-18 21:07:00 +0530

    resume page

--- myportfoliosite1/src/components/resume.js
+++ myportfoliosite1/src/components/resume.js
@@ -19,6 +19,8 @@ import {Grid,Cell} from 'react-mdl';
 
                               <h2 style={{paddingTop:'2em'}}>Isuru Dilshan</h2>
                               <h4 style={{color:'gray'}}>Programmer</h4>
+                              <hr style={{borderTop:'3px solid #833fb2',width:'50%'}}/>
+                              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                        </Cell>
 
                        <Cell className="resume-right-col" col={8}>Right side</Cell>
